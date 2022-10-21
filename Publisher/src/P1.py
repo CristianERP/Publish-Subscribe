05a6914d7f1e56c93b1a803df7abcc7beb33b8eb
Cristian Rojas <credrope@hotmail.com>
2022-10-21 01:13:28 -0500

    Actualizar Publisher

--- Publisher/src/P1.py
+++ Publisher/src/P1.py
@@ -20,17 +20,3 @@ channel.basic_publish(exchange=param_exchange, routing_key="", body=msg)
 print("Sent ", msg)
 
 connection.close()
-
-# import pika
-# import sys
-
-# connection = pika.BlockingConnection(
-#     pika.ConnectionParameters(host='localhost'))
-# channel = connection.channel()
-
-# channel.exchange_declare(exchange='logs', exchange_type='fanout')
-
-# message = ' '.join(sys.argv[1:]) or "info: Hello World!"
-# channel.basic_publish(exchange='logs', routing_key='', body=message)
-# print(" [x] Sent %r" % message)
-# connection.close()
--- Publisher/src/P2.js
+++ Publisher/src/P2.js
@@ -7,7 +7,6 @@ const publish = async () => {
     const channel = await connection.createChannel()
 
     await channel.assertExchange(exchange, 'fanout', {durable: false})
-    //await channel.assertQueue(queue)
 
     const msg = {
         "productor": "P2",
@@ -31,30 +30,3 @@ publish().catch( error => {
 })
 
 setTimeout(() => process.exit(0), 2000)
-
-
-// var amqp = require('amqplib/callback_api');
-
-// amqp.connect('amqp://localhost', function(error0, connection) {
-//     if (error0) {
-//         throw error0;
-//     }
-//     connection.createChannel(function(error1, channel) {
-//         if (error1) {
-//             throw error1;
-//         }
-//         var exchange = 'logs';
-//         var msg = process.argv.slice(2).join(' ') || 'Hello World!';
-
-//         channel.assertExchange(exchange, 'fanout', {
-//             durable: false
-//         });
-//         channel.publish(exchange, '', Buffer.from(msg));
-//         console.log(" [x] Sent %s", msg);
-//     });
-
-//     setTimeout(function() {
-//         connection.close();
-//         process.exit(0);
-//     }, 500);
-// });
